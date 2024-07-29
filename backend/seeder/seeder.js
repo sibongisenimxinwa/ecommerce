68545f846ff43fd564409a12286a396a74ff804f
sibongisenimxinwa <74898314+sibongisenimxinwa@users.noreply.github.com>
2024-07-29 13:57:14 +0200

    Update seeder.js

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,7 +4,7 @@ import Product from "../models/product.js";
 
 const seedProducts = async () => {
   try {
-    await mongoose.connect("mongodb+srv://root:mYb7nnW7W5RLcJNz@cluster0.xmygo.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("");
 
     await Product.deleteMany();
     console.log("Products are deleted");
